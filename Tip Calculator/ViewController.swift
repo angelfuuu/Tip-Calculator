82bc0f668b7fafd70cf639ec66e3f08c7bc8a5c5
angelfuuu <af2641@nyu.edu>
2020-09-11 16:33:51 -0400

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Anqi Fu on 9/11/20.
+//  Copyright © 2020 AngelFu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
